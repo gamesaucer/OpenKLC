a96f84f5605cbe380be0d1d6b70a792b50460aee
gamesaucer <gamesaucer@gmail.com>
2020-09-04 01:32:17 +0200

    Updated layout file to reduce redundant code.
    Additionally, I removed a line that caused a bug by overwriting
    previous edits to the module.

--- html/Utils/OKLCUtils/Layout.js
+++ html/Utils/OKLCUtils/Layout.js
@@ -1,10 +1,8 @@
-module.register('Layout', function (module = {}, Utils = window.Utils) {
-  window.OKLCUtils = module
-
+module.register('Layout', function (module = window.OKLCUtils, Utils = window.Utils, OKLCUtils = window.OKLCUtils) {
   /**
    * Representation of a keyboard layout
    */
-  module.Layout = class {
+  module.Layout = class Layout {
     constructor (metadata = {}, attributes = 0) {
       this.metadata = {
         kbd: 'Layout##',
@@ -17,34 +15,55 @@ module.register('Layout', function (module = {}, Utils = window.Utils) {
         ...metadata
       }
       this.attributes = attributes
+
+      const getDupeFilterFn = input => value => !input.some(o => o.is(value))
+      const splice = (array, valuesToRemove, valuesToAdd = []) =>
+        array.splice(0, 0, ...array.splice(0).filter(getDupeFilterFn(valuesToRemove)), ...valuesToAdd)
+
+      ;[OKLCUtils.KeyBinding, OKLCUtils.Char, OKLCUtils.Ligature, OKLCUtils.DeadKey, OKLCUtils.DeadKeyName].forEach(dataType => {
+        this[`layout${dataType.name}s`] = []
+        this[`add${dataType.name}s`] = function (...args) { splice(this[`layout${dataType.name}s`], args, args) }
+        this[`remove${dataType.name}s`] = function (...args) { splice(this[`layout${dataType.name}s`], args) }
+      })
     }
+
+    serialise () {}
+    static deserialise () {}
   }
 
   /**
    * Representation of a binding between a scancode and a virtual key
    */
-  module.KeyBinding = class {
+  module.KeyBinding = class KeyBinding {
     constructor (virtualKeyCode, scanCode) {
       this.virtualKeyCode = virtualKeyCode
       this.scanCode = scanCode
     }
+
+    serialise () {}
+    static deserialise () {}
+    is () {}
   }
 
   /**
    * Char table entry for a virtual key
    */
-  module.Char = class {
+  module.Char = class Char {
     constructor (virtualKey, flags, ...chars) {
       this.virtualKey = virtualKey
       this.flags = flags
       this.chars = chars
     }
+
+    serialise () {}
+    static deserialise () {}
+    is () {}
   }
 
   /**
    * Ligature table entry for a virtual key
    */
-  module.Ligature = class {
+  module.Ligature = class Ligature {
     constructor (virtualKey, modifierKeys, ...chars) {
       this.virtualKey = virtualKey
       this.modifierKeys = modifierKeys
@@ -52,12 +71,30 @@ module.register('Layout', function (module = {}, Utils = window.Utils) {
 
       Utils.Errors.StringLength('Expected text of length %2-%3 but received length %1', chars, 1, 4)
     }
+
+    serialise () {}
+    static deserialise () {}
+    is () {}
+  }
+
+  /**
+   * Dead key name table entry
+   */
+  module.DeadKeyName = class DeadKeyName {
+    constructor (accentChar, name) {
+      this.accentChar = accentChar
+      this.name = name
+    }
+
+    serialise () {}
+    static deserialise () {}
+    is () {}
   }
 
   /**
    * Dead key table entry
    */
-  module.DeadKey = class {
+  module.DeadKey = class DeadKey {
     constructor (accentChar, baseChar, compositeChar, dead) {
       this.accentChar = accentChar
       this.baseChar = baseChar
@@ -67,5 +104,9 @@ module.register('Layout', function (module = {}, Utils = window.Utils) {
       ;[accentChar, baseChar, compositeChar].forEach(char =>
         Utils.Errors.StringLength('Expected a single character but received text of length %1', char, 1, 1))
     }
+
+    serialise () {}
+    static deserialise () {}
+    is () {}
   }
 }, ['Utils', 'OKLCUtils'])
